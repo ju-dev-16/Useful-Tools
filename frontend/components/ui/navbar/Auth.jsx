414765a27bb1863625b5cfccc4456bfc55c95f41
JuDev <jahidudd65@gmail.com>
2022-12-15 19:13:26 +0100

    Implement basic auth with Github

--- frontend/components/ui/navbar/Auth.jsx
+++ frontend/components/ui/navbar/Auth.jsx
@@ -1,19 +1,26 @@
-import { useRouter } from "next/router";
+import { useSession, signIn, signOut } from "next-auth/react";
 
 import Container from "react-bootstrap/Container";
 import Button from "react-bootstrap/Button";
 
-export default function Auth () {
-  const router = useRouter();
+export default function Auth() {
+  const { data: session } = useSession();
+
+  const AuthButton = () => {
+    if (session) {
+      return <Button onClick={() => signOut()}>Sign out</Button>;
+    }
+    
+    return (
+      <Button onClick={() => signIn()}>
+        Sign in
+      </Button>
+    );
+  }
 
   return (
     <Container className="d-flex justify-content-center">
-      <Button className='me-2' onClick={() => router.push("/auth/login")}>
-        Login
-      </Button>
-      <Button className='btn-danger' onClick={() => router.push("/auth/register")}>
-        Register
-      </Button>
+      <AuthButton />
     </Container>
   );
 }
--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,14 +1,18 @@
 import '../styles/globals.css';
-
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { SessionProvider } from 'next-auth/react';
 
 import CustomNavbar from '../components/ui/navbar/CustomNavbar';
 
-export default function MyApp({ Component, pageProps }) {
+export default function MyApp({ 
+  Component, 
+  pageProps: { session, ...pageProps}
+}) {
   return (
-    <>
+    <SessionProvider session={session}>
       <CustomNavbar />
       <Component {...pageProps} />
-    </>
+    </SessionProvider>
   );
 }
